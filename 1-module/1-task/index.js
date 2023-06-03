390cbfc69137f239112dae155c0b8d0dde30a019
tr4nk <tr4nk.kol@gmail.com>
2023-06-03 15:26:28 +0300

    функция для вычесления факториала числа

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,13 @@
 function factorial(n) {
-  // ваш код...
+  let result = 1
+  for (i = 1 ; i<= n; i++){
+    result = result * i
+  }
+  return result
 }
+console.log(factorial(0))
+console.log(factorial(1))
+console.log(factorial(2))
+console.log(factorial(3))
+console.log(factorial(4))
+console.log(factorial(5))
