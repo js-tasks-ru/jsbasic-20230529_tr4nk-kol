c15b26e28ad04011c4b8b9dd7f5c9afdce8c7843
tr4nk <tr4nk.kol@gmail.com>
2023-06-04 10:03:53 +0300

    функция меняющая первый символ на заглавный

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,11 @@
 function ucFirst(str) {
-  // ваш код...
+  if(!str ){
+    return ''
+    // return false
+  }
+
+ return str[0].toUpperCase()  + str.slice(1)
 }
+console.log(ucFirst('вася'))
+console.log(ucFirst('в'))
+console.log(ucFirst(''))
